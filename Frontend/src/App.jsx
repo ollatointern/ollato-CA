4378a5ac41b11a3c08ab85ee1c1e78578c6140ed
Zahabiya Tawawala <zahabiyatawawala1@gmail.com>
2024-08-04 18:25:50 +0530

    minor changes in app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './auth/Login.jsx';
 import Register from './auth/Register.jsx';
 import Student from './studentPanel/Student.jsx';
-import Councellor from './councellorPanel/Counsellor.jsx';
+import Counsellor from './councellorPanel/Counsellor.jsx';
 import Center from './centerPanel/Center.jsx';
 import Admin from './adminPanel/Admin.jsx';
 
@@ -14,7 +14,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/student" element={<Student />} />
-          <Route path="/councellor" element={<Councellor />} />
+          <Route path="/counsellor" element={<Counsellor />} />
           <Route path="/center" element={<Center />} />
           <Route path="/admin" element={<Admin />} />
         </Routes>
