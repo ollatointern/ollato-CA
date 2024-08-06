4cd137e0b90bd501056036fa7954bd0946fe7878
Zahabiya Tawawala <zahabiyatawawala1@gmail.com>
2024-08-06 09:58:43 +0530

    modified app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-        {/* <Route path="/" element={<Navigate to="/login" replace />} /> */}
+          <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/student/*" element={<Student />} />
